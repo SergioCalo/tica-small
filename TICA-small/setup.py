42015f4cef2ee4fc9bbc1ba6acde3062def0ad1f
SergioCalo <60823800+SergioCalo@users.noreply.github.com>
2021-12-20 11:34:22 +0100

    Update setup.py

--- TICA-small/setup.py
+++ TICA-small/setup.py
@@ -3,9 +3,6 @@ import setuptools
 setuptools.setup(
     name="streamlit-bokeh-events",
     version="0.1.3",
-    author="Ashish Shukla",
-    author_email="ash2shukla@gmail.com",
-    description="A custom streamlit component to return js event values from bokeh plots to streamlit",
     long_description="",
     long_description_content_type="text/plain",
     url="",
